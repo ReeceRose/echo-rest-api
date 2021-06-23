49cebc7dbfc02cb75c0efbf3ac52456a12cac579
Reece Rose <reecerose18@gmail.com>
2021-06-23 13:04:29 -0400

    Setup database connection via gorm. Created book model. Added github action

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Echo REST API
 
 Simple REST API made in Go with the [echo](https://github.com/labstack/echo) web framework.
+
+## About
+
+This is a re-creation of [Fiber REST API](https://github.com/ReeceRose/fiber-rest-api), which is a REST API for books that uses the Fiber web framework.
--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo/v4"
+
+	"github.com/reecerose/echo-rest-api/database"
+	"github.com/reecerose/echo-rest-api/models"
 )
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	fmt.Println("Datbase connection successfully opened")
+
+	// Auto migrate DB
+	database.DBConn.AutoMigrate(&models.Book{})
+	fmt.Println("Database migrated")
+}
 
 func main() {
 	e := echo.New()
+
+	initDatabase()
+	defer database.DBConn.Close()
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
